e4de227488bcfe5a1fba020e9c7d72c5b6774009
LuketeDev <lulukas.2023@gmail.com>
2026-04-20 15:05:38 -0300

    first commit

---
+++ src/test/java/io/github/lukete/serviceflow/ServiceflowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lukete.serviceflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
