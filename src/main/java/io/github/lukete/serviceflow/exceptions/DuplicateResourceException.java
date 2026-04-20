2d68025b9e681d2a3ad62f874e6bfa35dbeb2b70
LuketeDev <lulukas.2023@gmail.com>
2026-04-20 19:41:06 -0300

    feat(exceptions): add exception for duplicate resources

---
+++ src/main/java/io/github/lukete/serviceflow/exceptions/DuplicateResourceException.java
@@ -0,0 +1,8 @@
+package io.github.lukete.serviceflow.exceptions;
+
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
