801de6d9dedcc45cdf30aa43b57980428e7c5461
LuketeDev <lulukas.2023@gmail.com>
2026-04-20 18:50:45 -0300

    feat: update base entity

--- src/main/java/io/github/lukete/serviceflow/common/BaseEntity.java
+++ src/main/java/io/github/lukete/serviceflow/common/BaseEntity.java
@@ -4,6 +4,13 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class BaseEntity {
     @Column
     private LocalDateTime createdAt;
