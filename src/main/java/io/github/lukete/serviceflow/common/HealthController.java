0746eef4d4ecf97e590d3b5b622bc1822cf7f3ed
LuketeDev <lulukas.2023@gmail.com>
2026-04-20 15:28:43 -0300

    feat(health): add health endpoint

---
+++ src/main/java/io/github/lukete/serviceflow/common/HealthController.java
@@ -0,0 +1,15 @@
+package io.github.lukete.serviceflow.common;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "ServiceFlow is running";
+    }
+
+}
