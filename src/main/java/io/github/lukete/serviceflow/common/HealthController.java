5cc6b3555df2aeb70e01946eac34d20f1eeba83d
LuketeDev <lulukas.2023@gmail.com>
2026-04-27 12:09:22 -0300

    feat(auth-test): create me endpoint for auth test

--- src/main/java/io/github/lukete/serviceflow/common/HealthController.java
+++ src/main/java/io/github/lukete/serviceflow/common/HealthController.java
@@ -2,7 +2,13 @@ package io.github.lukete.serviceflow.common;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.lukete.serviceflow.user.domain.entity.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")
@@ -10,6 +16,12 @@ public class HealthController {
     @GetMapping("/health")
     public String health() {
         return "ServiceFlow is running";
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> me(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(user.getEmail());
     }
 
 }
